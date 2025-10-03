e57d8d8fa320eafb8a0d36fff5102b1e1ea6b286
Creep7er <creep7er@proton.me>
2025-10-03 10:52:26 +0500

    Categories are needed

---
+++ Абрамян_задачи/For/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int K;
+    int N;
+    cout << "Input K:";
+    cin >> K;
+    cout << "Input N:";
+    cin >> N;
+    
+    for (int i = 0; i < K;){
+        cout << N;
+        i++;
+        
+    }
+
+    return 0;
+}
